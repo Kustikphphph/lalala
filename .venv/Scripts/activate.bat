e81deb4561fee63bbdfff1ced4fdbaa81bb7656d
Kustikphphph <puppy.maria@yandex.ru>
2024-11-30 22:41:07 +0300

    30.11.2024 лз23Создание Django API используя Django Rest Framework, часть 2.

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\puppy\Desktop\dtf\lalala\.venv
+set VIRTUAL_ENV=C:\Users\puppy\Desktop\SDFGH\lalala\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
